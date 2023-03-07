7f646cfcf6ad206fb121980ae3b7792989717b6e
Kalininsky-Mark <124485970+Kalininsky-Mark@users.noreply.github.com>
2023-03-07 16:27:34 +0300

    Delete base-sintax/src/ru/sber/OOP1 directory

--- base-sintax/src/ru/sber/OOP1/Task2.java
+++
@@ -1,8 +0,0 @@
-package ru.sber.OOP1;
-
-public class Task2 {
-    public static void main(String[] args) {
-        Circle circle = new Circle(1, "red");
-        System.out.println(circle.toString());
-    }
-}
